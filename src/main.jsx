30cb9bdf4ec5de4c1136c0f4d0c77a5902c0900f
nicholas-ackley <155398375+nicholas-ackley@users.noreply.github.com>
2024-06-10 16:54:18 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home.jsx';
-import Test from './Test.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
 import './index.css'
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/test", element: <Test /> },
-  // { path: "/music", element <Music/>}
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
