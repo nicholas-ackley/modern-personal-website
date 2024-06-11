d17bf515a08f28cef6ab2de089d113507007a657
nicholas-ackley <155398375+nicholas-ackley@users.noreply.github.com>
2024-06-11 17:41:06 -0500

    Add files via upload

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,19 @@ import './Home.css';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar.jsx'
 import Me from './images/me.png';
+import { useState, useEffect } from 'react';
+
 function Home() {
   const navigate = useNavigate();
+  const message = 'Front End Developer';
+  let index = 0;
+
+  // const [currentText, setCurrentText] = useState('');
+      // const [First, setFirst] = useState(false);
+
+  useEffect(() =>{
+    console.log(message.charAt(0))
+  },[])
 
   const handleNavigate = () => {
     navigate("/test");
@@ -11,23 +22,42 @@ function Home() {
 
   return (
     <>
-    <Navbar/>
-      {/* <h1 onClick={handleNavigate}>Hello World!</h1> */}
-      <div className="header-name"><p>nicholasAckley();</p></div>
+    {/* <Navbar/> */}
+      <div className="header-name"><p>nicholasAckley();</p><Navbar/><div className="animation" ></div></div>
       <div className="body">
         <div className="bio-container">
-            <div className="name"><h1> Hi, I'm Nicholas Ackley</h1></div>
-              <div className="front-end-text"><b>Front-End Developer</b></div>
+            <div className="name"><h1><b> Hi, I'm Nicholas Ackley</b></h1> <div className="animation"></div></div>
+              <div className="front-end-text">Front-End Developer
+                <div className="animation"></div>
+              </div>
                 <div className="biography"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-                book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with 
-                the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem 
-                Ipsum.</p>
+                book. .</p><div className="animation"></div>
               </div>
+              <div className="button-container1">
+                <button className='linked-in'>Old Website </button>
+              <div className="animation"></div>
+              </div>
+              <div className="button-container2">
+                <button className='git-hub'>GitHub</button>
+              <div className="animation"></div>
+              </div>
+
             <img src={Me} className='circle-object'></img>
+        </div>
+        <div className="social-media-container">
+          <div className="social-media-circle"><box-icon className='gram'
+            name='instagram' 
+            type='logo' 
+            rotate='90' 
+            color='#00abf0'></box-icon>
+          </div>
+          <div className="social-media-circle"><box-icon name='linkedin' type='logo' rotate='360' color='#00abf0' ></box-icon></div>
+          <div className="social-media-circle"><box-icon name='github' type='logo' color='#00abf0' ></box-icon></div>
         </div>
         
       </div>
+      
     </>
   )
 }
